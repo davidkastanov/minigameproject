48ec5f7d2e5301e83a56ba78e4a936646693487c
Anttoha <167896136+Antoha2012pro@users.noreply.github.com>
2025-09-09 17:29:07 +0200

    fix bug and exerises

--- src/js/exerises.js
+++ src/js/exerises.js
@@ -1,95 +1,96 @@
-const scientists = [ 
-    { 
-        name: "Albert", 
-        surname: "Einstein", 
-        born: 1879, 
-        dead: 1955, 
-        id: 1 
-    }, 
-    { 
-        name: "Isaac", 
-        surname: "Newton", 
-        born: 1643, 
-        dead: 1727, 
-        id: 2 
-    }, 
-    { 
-        name: "Galileo", 
-        surname: "Galilei", 
-        born: 1564, 
-        dead: 1642, 
-        id: 3 
-    }, 
-    { 
-        name: "Marie", 
-        surname: "Curie", 
-        born: 1867, 
-        dead: 1934, 
-        id: 4 
-    }, 
-    { 
-        name: "Johannes", 
-        surname: "Kepler", 
-        born: 1571, 
-        dead: 1630, 
-        id: 5 
-    }, 
-    { 
-        name: "Nicolaus", 
-        surname: "Copernicus", 
-        born: 1473, 
-        dead: 1543, 
-        id: 6 
-    }, 
-    { 
-        name: "Max", 
-        surname: "Planck", 
-        born: 1858, 
-        dead: 1947, 
-        id: 7 
-    }, 
-    { 
-        name: "Katherine", 
-        surname: "Blodgett", 
-        born: 1898, 
-        dead: 1979, 
-        id: 8 
-    }, 
-    { 
-        name: "Ada", 
-        surname: "Lovelace", 
-        born: 1815, 
-        dead: 1852, 
-        id: 9 
-    }, 
-    { 
-        name: "Sarah E.", 
-        surname: "Goode", 
-        born: 1855, 
-        dead: 1905, 
-        id: 10 
-    }, 
-    { 
-        name: "Lise", 
-        surname: "Meitner", 
-        born: 1878, 
-        dead: 1968, 
-        id: 11 
-    }, 
-    { 
-        name: "Hanna", 
-        surname: "Hammarström", 
-        born: 1829, 
-        dead: 1909, 
-        id: 12 
-    } 
+const scientists = [
+    {
+        name: "Albert",
+        surname: "Einstein",
+        born: 1879,
+        dead: 1955,
+        id: 1
+    },
+    {
+        name: "Isaac",
+        surname: "Newton",
+        born: 1643,
+        dead: 1727,
+        id: 2
+    },
+    {
+        name: "Galileo",
+        surname: "Galilei",
+        born: 1564,
+        dead: 1642,
+        id: 3
+    },
+    {
+        name: "Marie",
+        surname: "Curie",
+        born: 1867,
+        dead: 1934,
+        id: 4
+    },
+    {
+        name: "Johannes",
+        surname: "Kepler",
+        born: 1571,
+        dead: 1630,
+        id: 5
+    },
+    {
+        name: "Nicolaus",
+        surname: "Copernicus",
+        born: 1473,
+        dead: 1543,
+        id: 6
+    },
+    {
+        name: "Max",
+        surname: "Planck",
+        born: 1858,
+        dead: 1947,
+        id: 7
+    },
+    {
+        name: "Katherine",
+        surname: "Blodgett",
+        born: 1898,
+        dead: 1979,
+        id: 8
+    },
+    {
+        name: "Ada",
+        surname: "Lovelace",
+        born: 1815,
+        dead: 1852,
+        id: 9
+    },
+    {
+        name: "Sarah E.",
+        surname: "Goode",
+        born: 1855,
+        dead: 1905,
+        id: 10
+    },
+    {
+        name: "Lise",
+        surname: "Meitner",
+        born: 1878,
+        dead: 1968,
+        id: 11
+    },
+    {
+        name: "Hanna",
+        surname: "Hammarström",
+        born: 1829,
+        dead: 1909,
+        id: 12
+    }
 ];
 
 const exerItemsEl = document.querySelector('.exerises__items');
 const exerButtonsEls = document.querySelectorAll('.exerises__question-button');
+const exerWarnEl = document.querySelector('.exerises__warn');
 
 const exerCreateItems = (array) => {
-    exerItemsEl.innerHTML = array.map(({name, surname, born, dead}) => `
+    exerItemsEl.innerHTML = array.map(({ name, surname, born, dead }) => `
             <li class="exerises__item">
                 <h3 class="exerises__item-name">${name}</h3>
                 <h4 class="exerises__item-surname">${surname}</h4>
@@ -101,6 +102,8 @@ const exerCreateItems = (array) => {
 
 exerButtonsEls.forEach(btn => {
     btn.addEventListener('click', (event) => {
+        exerWarnEl.classList.add('black')
+        exerWarnEl.textContent = '';
         const action = event.currentTarget.dataset.action;
         switch (action) {
             case 'born-19-st':
@@ -108,16 +111,26 @@ exerButtonsEls.forEach(btn => {
                 exerCreateItems(bornIn19St);
                 break;
             case 'born-albert':
-
+                const albert = scientists.find(albert => albert);
+                exerWarnEl.classList.add('green');
+                exerWarnEl.textContent = `${albert.name} ${albert.surname} народився у ${albert.born} році`;
+                exerCreateItems(scientists);
                 break;
             case 'alphabet':
-
+                const alphabet = [...scientists].sort((a, b) => a.name.localeCompare(b.name));
+                exerCreateItems(alphabet);
                 break;
             case 'surname-first-c':
-
+                const surnameFirstC = scientists.filter(s => s.surname.toUpperCase().startsWith('C'))
+                exerCreateItems(surnameFirstC);
                 break;
             case 'born-dead-years':
-
+                const bornDeadYears = [...scientists].sort((a, b) => {
+                    const lifeA = a.dead - a.born;
+                    const lifeB = b.dead - b.born;
+                    return lifeA - lifeB;
+                });
+                exerCreateItems(bornDeadYears);
                 break;
             case 'name-first-a':
 
@@ -133,24 +146,22 @@ exerButtonsEls.forEach(btn => {
                 break;
             case 'worked-exerises-19-st':
                 const workedExerises19St = scientists.every(s => s.born + 18 >= 1800 && s.born <= 1900);
+                exerCreateItems(scientists);
                 if (workedExerises19St) {
-                    alert('Всі працювали!');
+                    exerWarnEl.classList.add('green');
+                    exerWarnEl.textContent = `Всі працювали!`;
+                    break;
                 } else {
-                    alert('Не всі працювали.');
+                    exerWarnEl.classList.add('red');
+                    exerWarnEl.textContent = `Не всі працювали.`;
                 }
                 break;
-//            отримати масив вчених що народилися в 19 ст
-// знайти суму років скільки прожили всі вченні
-//            Відсортувати вчених по алфавіту
-//            Відсортувати вчених по кількості прожитих років
-// Видалити з масива вчених що народилися в 15 або 16 або 17 столітті
-//             Знайти вченого який народився найпізніше.
-//             Знайти рік народження Albert Einstein
-//             знайти вчених прізвище яких починається на літеру С
-//             Видалити з масива всіх вчених імя яких починається на A
-//             Знайти вченого який прожив найбільше і вченого який прожив найменьше
-// Знатйи вчених в яких співпадають перші літери імені і прізвища
-//             Дізнатися чи всі вченні працювали в 19 столітті
+            case 'sum-ages':
+
+                break;
+            case 'remove-born-15-17':
+
+                break;
         }
     })
 })
